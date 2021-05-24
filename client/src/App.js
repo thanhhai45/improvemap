45d338e922b7381aa088047d119866f31669cb04
thanhhai45 <thanhhai82451495@gmail.com>
2021-05-24 10:42:40 +0700

    completed feature and deploy

--- client/src/App.js
+++ client/src/App.js
@@ -11,15 +11,15 @@ function App() {
   return (
     <AuthContextProvider>
       <PostContextProvider>
-      <Router>
-        <Switch>
-          <Route exact path='/' component={Landing} />
-          <Route exact path='/login' render={props => <Auth {...props} authRoute='login' />}/>
-          <Route exact path='/register' render={props => <Auth {...props} authRoute='register' />}/>
-          <ProtectedRoute exact path='/dashboard' component={Dashboard} />
-          <ProtectedRoute exact path='/about' component={About} />
-        </Switch>
-      </Router>
+        <Router>
+          <Switch>
+            <Route exact path='/' component={Landing} />
+            <Route exact path='/login' render={props => <Auth {...props} authRoute='login' />}/>
+            <Route exact path='/register' render={props => <Auth {...props} authRoute='register' />}/>
+            <ProtectedRoute exact path='/dashboard' component={Dashboard} />
+            <ProtectedRoute exact path='/about' component={About} />
+          </Switch>
+        </Router>
       </PostContextProvider>
     </AuthContextProvider>
   )
--- client/src/reducers/postReducer.js
+++ client/src/reducers/postReducer.js
@@ -8,6 +8,36 @@ export const postReducer = (state, action) => {
         posts: payload,
         postLoading: false,
       }
+    case 'POST_LOADED_FAIL':
+      return {
+        ...state,
+        posts: [],
+        postLoading: false,
+      }
+    case 'ADD_POST':
+      return {
+        ...state,
+        posts: [...state.posts, payload],
+      }
+    case 'DELETE_POST':
+      return {
+        ...state,
+        posts: state.posts.filter(post => post._id !== payload )
+      }
+    case 'FIND_POST':
+      return {
+        ...state,
+        post: payload 
+      }
+    case 'UPDATE_POST':
+      const newPosts = state.posts.map(post => {
+        if (post._id === payload._id) return payload
+        return post
+      })
+      return {
+        ...state,
+        posts: newPosts
+      }
     default:
       return state
   }
